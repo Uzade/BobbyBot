0fc8e5466048be5bcabde654108380021d3ced8b
Joschka <joschka4@gmx.de>
2023-02-22 20:27:34 +0100

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+const { token } = require('./config.json')
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+const UID = '462968407042686976' // The recipients user ID => Change to yours
+
+var express = require('express');
+var app = express();
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(token)
+
+app.get('/sendMessage', function (req, res) {
+    if(!req.query.amount || !req.query.name){
+        console.log(req.query.amount, req.query.name)
+        res.status(400).end('Invalid params!\nMake sure to add amount and name to the request')
+        return
+    }
+    client.users.fetch(UID, false).then((user) => {
+        user.send(`Gib Bobby jetzt ${req.query.amount} Karotten ! Sofort! \nDas ist ein Auftrag von ${req.query.name}`)
+       })
+   res.end('message send')
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ frontend/BobbyApp/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+
+	let name = '';
+	let amount = '';
+
+	const sendRequest = () => {
+		fetch(`http://127.0.0.1:8081/sendMessage?name=${name}&amount=${amount}`)
+	}
+	
+</script>
+
+<main>
+	<h1>Send Bobby Food!</h1>
+	<form on:submit|preventDefault={sendRequest}>
+		<label for="name">Dein Name:</label>
+		<input bind:value={name} id="name">
+		<label for="amount">Wie viel Karotten soll Bobby bekommen?</label>
+		<input bind:value={amount} id="value">
+		<br/>
+		<button type="submit">
+			Send to Bobby
+		</button>
+	</form>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
